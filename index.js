75ecbacb20c4693016f95379f828e8f2ed1a8ebf
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-06-10 16:51:36 -0700

    reworked classes to have only one property and had the rest directly inserted into SVG doc, connected the generate function to index, add writefile, added SVG format and render function

--- index.js
+++ index.js
@@ -1,58 +1,73 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { Circle, Triangle, Square } = require("./lib/shapes");
 
 const questions = [
   {
     type: "input",
-    name: "characters",
+    name: "character",
     message: "What text would you like on your SVG? (Max: 3 characters)",
   },
 
   {
-    type: "list",
-    name: "colors",
-    message: "What color would you like to would you like the characters",
-    choices: [
-      "Red",
-      "Blue",
-      "Green",
-      "Purple",
-      "orange",
-      "Pink",
-      "Yellow",
-      "Brown",
-      "Black",
-    ],
+    type: "input",
+    name: "characterColor",
+    message:
+      "Please enter a color keyword for your text color!(OR a hexadecimal number) ",
   },
   {
     type: "list",
-    name: "shapes",
+    name: "shape",
     message: "What Shape would you like?",
     choices: ["Circle", "Triangle", "Square"],
   },
   {
-    type: "list",
-    name: "shapeColors",
-    message: "What color would you like to would your shape?",
-    choices: [
-      "Red",
-      "Blue",
-      "Green",
-      "Purple",
-      "orange",
-      "Pink",
-      "Yellow",
-      "Brown",
-      "Black",
-    ],
+    type: "input",
+    name: "shapeColor",
+    message:
+      "Please enter a color keyword for your shape color!(OR a hexadecimal number) ",
   },
 ];
 
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName, render(data), (err) =>
-    err ? console.log(err) : console.log(`Success`)
-  );
+function generateSVG(data) {
+  let shape = data.shape;
+  console.log(shape);
+  switch (shape) {
+    case "Circle":
+      shape = new Circle();
+      shape.setColor(data.shapeColor);
+      break;
+    case "Triangle":
+      shape = new Triangle();
+      shape.setColor(data.shapeColor);
+      break;
+    case "Square":
+      shape = new Square();
+      shape.setColor(data.shapeColor);
+      break;
+  }
+
+  return `
+<svg version="1.1"
+width="300" height="200"
+xmlns="http://www.w3.org/2000/svg">
+
+<rect width="300px" height="200px" fill="white" />
+
+${shape.render()}
+
+<text x="50%" y="60%" font-size="50" text-anchor="middle" cominant-baseline = "center" fill="${
+    data.characterColor
+  }">${data.character}</text>
+
+</svg>`;
 }
+
+writeToFile = (fileName, data) => {
+  fs.writeFile(fileName, generateSVG(data), (err) =>
+    err ? console.log(err) : console.log(`Generated logo.svg`)
+  );
+};
 
 function init() {
   inquirer
